4bb0b165ced33745a1c6c781198ff992065d26cd
Mark Schmale <masch@masch.it>
2014-04-05 07:34:30 +0200

    changed test framework to tape

--- test/data.js
+++ test/data.js
@@ -1,4 +1,6 @@
 module.exports = {
     binKeyData: new Buffer("ZDU6ZmlsZXNkMjA6N7VVuuCjmp5LoM+n15a5iM/XJHdkODpjb21wbGV0ZWkwZTEwOmRvd25sb2FkZWRpMTBlMTA6aW5jb21wbGV0ZWkwZWVlZQ==", 'base64')
   , binKeyName: (new Buffer("N++/vVXvv73go5rvv71L77+9z6fXlu+/ve+/ve+/ve+/vSR3", 'base64')).toString()
+  , binStringData: new Buffer('w7bCsXNkZg==', 'base64')
+  , binResultData: new Buffer('NzrDtsKxc2Rm', 'base64')
 }
