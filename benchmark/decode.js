c2a3ff6cd41c9039cdbccfd92ab99b7798b8bf9a
Kirill Fomichev <fanatid@ya.ru>
2016-03-20 12:30:47 +0300

    update benchmarks

--- benchmark/decode.js
+++ benchmark/decode.js
@@ -1,44 +1,18 @@
-var fs        = require( 'fs' )
+/* global suite, bench */
+var fs = require('fs')
 
-var bencode     = require( '../' )
-var bencoding   = require( 'bencoding' )
-var dht_bencode = require( 'dht-bencode' )
-var bncode      = require( 'bncode' )
-var dht         = require( 'dht.js/lib/dht/bencode' )
+var bencode = require('../')
+var bencoding = require('bencoding')
+var dht_bencode = require('dht-bencode')
+var bncode = require('bncode')
+var dht = require('dht.js/lib/dht/bencode')
 
-var buffer = fs.readFileSync( __dirname + '/test.torrent' )
+var buffer = fs.readFileSync(__dirname + '/test.torrent')
 
-suite('decode to buffer', function() {
-    bench('bencode', function() {
-        bencode.decode( buffer )
-    })
-    bench('bencoding', function() {
-        var v = bencoding.decode( buffer )
-    })
-    bench('dht_bencode', function() {
-        dht_bencode.bdecode( buffer )
-    })
-    bench('bncode', function() {
-        bncode.decode( buffer )
-    })
-    bench('dht', function() {
-        dht.decode( buffer )
-    })
+suite('decode to buffer', () => {
+  bench('bencode', () => bencode.decode(buffer))
+  bench('bencoding', () => bencoding.decode(buffer))
+  bench('dht_bencode', () => dht_bencode.bdecode(buffer))
+  bench('bncode', () => bncode.decode(buffer))
+  bench('dht', () => dht.decode(buffer))
 })
-
-suite('decode to utf8', function() {
-    bench('bencode', function() {
-        bencode.decode( buffer, 'utf8' )
-    })
-})
-suite('decode to ascii', function() {
-    bench('bencode', function() {
-        bencode.decode( buffer, 'ascii' )
-    })
-})
-suite('decode to binary', function() {
-    bench('bencode', function() {
-        bencode.decode( buffer, 'ascii' )
-    })
-})
-
