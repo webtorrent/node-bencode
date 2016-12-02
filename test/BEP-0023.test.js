b31e00d5eb73b17ae6b507194706e21cf1f70985
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-12-02 17:35:37 +0100

    Add test/BEP-0023: Test correct handling of compacted peer lists
    Ref #54

---
+++ test/BEP-0023.test.js
@@ -0,0 +1,41 @@
+var bencode = require('..')
+var path = require('path')
+var fs = require('fs')
+var test = require('tape').test
+
+// @see http://www.bittorrent.org/beps/bep_0023.html
+test('BEP 0023', function (t) {
+  t.test('should be able to handle an compacted peer announce', function (t) {
+    var filename = path.join(__dirname, 'data', 'announce-compacted-peers.bin')
+    var announce = fs.readFileSync(filename)
+    var data = bencode.decode(announce)
+
+    console.log(data)
+
+    t.plan(1)
+    t.deepEqual(data, {
+      complete: 4,
+      incomplete: 3,
+      interval: 1800,
+      'min interval': 1800,
+      peers: new Buffer('2ebd1b641a1f51d54c0546cc342190401a1f626ee9c6c8d5cb0d92131a1fac4e689a3c6b180f3d5746db', 'hex')
+    })
+  })
+
+  t.test('should be able to handle an compacted peer announce when decoding strings', function (t) {
+    var filename = path.join(__dirname, 'data', 'announce-compacted-peers.bin')
+    var announce = fs.readFileSync(filename)
+    var data = bencode.decode(announce, 'utf8')
+
+    console.log(data)
+
+    t.plan(1)
+    t.deepEqual(data, {
+      complete: 4,
+      incomplete: 3,
+      interval: 1800,
+      'min interval': 1800,
+      peers: '.�\u001bd\u001a\u001fQ�L\u0005F�4!�@\u001a\u001fbn�����\r�\u0013\u001a\u001f�Nh�<k\u0018\u000f=WF�'
+    })
+  })
+})
